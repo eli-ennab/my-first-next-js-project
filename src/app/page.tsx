ef046cd9e9473b5c5288d7fb030cfd0a2879bd32
Eli Ennab <elinaennab@gmail.com>
2023-07-06 16:29:21 +0200

    make Home() async and await todos with prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <h1>Hi</h1>
+import { prisma } from "@/db";
+import Link from "next/link";
+
+export default async function Home() {
+
+  const todos = await prisma.todo.findMany()
+
+  return <>
+    <header className="flex justify-between
+    items-center mb-4">
+      <h1 className="text-2xl">Todos</h1>
+      <Link 
+        className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-wihtin:bg-slate-700 outline-none"
+        href="/new"
+      >
+            New
+      </Link>
+    </header>
+    <ul className="pl-4">
+      {todos.map(todo => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  </>
 }
